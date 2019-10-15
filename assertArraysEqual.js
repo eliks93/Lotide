774b0c76df0be4cc8fb4c26278fb755544b11a2f
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-15 22:13:42 +0000

    added assertEqualArray fucntion

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const eqArrays = function(array1, array2) {
+  let string1 = "";
+  let string2 = "";
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++){
+    string1 += array1[i];
+    string2 += array2[i];
+    if (typeof array1[i] !== typeof array2[i]){
+      return false;
+    }
+  }
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertEqual = function(input1, input2) {
+  let answer = eqArrays(input1, input2)
+  if (answer === true) {
+    console.log(`✅✅✅ Assertion Passed: ${input1} === ${input2}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${input1} !== ${input2}`);
+  }
+};
+
+assertEqual(["one", 2, 3], ["one", 2, 3]);
