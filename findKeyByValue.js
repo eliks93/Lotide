5c7c034cec0de4fd43b628032e676be69f20d8fe
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 22:25:13 +0000

    added find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const findKeyByValue = function(object, value) {
+  for (let property in object) {
+    if(value === object[property]) {
+      return(property);
+    }
+  }
+}
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// test cases and object
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
