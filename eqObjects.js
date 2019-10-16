3fae53e3d2d765eb9889490830032207a1030d63
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 22:33:24 +0000

    added basic test of one true and one false condition

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -11,3 +5,16 @@ const assertEqual = function(actual, expected) {
     console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+
+assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(ab, ba), true);
