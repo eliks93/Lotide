26decc45cd579ca6b66f96cdf1d198dbbffb9f69
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 20:13:42 +0000

    ensured white space was not returned as a key pair

--- countLetters.js
+++ countLetters.js
@@ -1,50 +1,17 @@
 const countLetters = function(string) {
-  const lowerString = string.toLowerCase();
+  const lowerString = string.toLowerCase().split(" ").join("");
   const results = {};
-  for(const letters of lowerString) {
-    if (results[letters]) {
-      results[letters] += 1;
-    } else {
-      results[letters] = 1;
-    }
+  for (const letters of lowerString) {
+    
+      if (results[letters] !== undefined) {
+          results[letters] += 1;
+        } else {
+          results[letters] = 1;
+      }
+
   }
-  console.log(results)
   return results;
 };
-
-
-string = "Lighthouse Labs";
-
-
-
-
-result1 = countLetters(string) 
-
-assertEqual(result1["a"], 1);
-assertEqual(result1["l"], 2);
-assertEqual(result1["z"], undefined);
-assertEqual(result1[" "], undefined);
-assertEqual(result1["h"], 2);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Test Code 
 const assertEqual = function(actual, expected) {
@@ -55,3 +22,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 // Test Below here
+string = "Lighthouse Labs";
+
+result1 = countLetters(string);
+console.log(result1);
+assertEqual(result1["a"], 1);
+assertEqual(result1["l"], 2);
+assertEqual(result1["z"], undefined);
+assertEqual(result1[" "], undefined);
+assertEqual(result1["h"], 2);
