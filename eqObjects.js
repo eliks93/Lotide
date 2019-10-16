d16b6cce47c05e1ee01dae9fc93dff6b23a80c34
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 22:30:11 +0000

    added assertEqual and declared eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
