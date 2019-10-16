776ccd45872f21848a0f6691445c4a83a9c91132
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 23:22:18 +0000

    function passes primitive tests

--- eqObjects.js
+++ eqObjects.js
@@ -9,12 +9,32 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  let array1 = Object.keys(object1);
+  let array2 = Object.keys(object2);
+  if(Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  } 
+  for(let key1 of array1){
+    
+    if(object1[key1] !== object2[key1]){
+      return false;
+    }
+    
+    }
+  return true
 };
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
+const cba = { c: "3", b: "2", a: "1" };
+const cd = {c: "1", d: "2"};
+const dc = {d: "2", c: "1"};
 
-assertEqual(eqObjects(ab, abc), false);
+
 assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(dc, cd), true);
+assertEqual(eqObjects(abc, cba), true);
+
