e0a42799e518b9d97491daa14ed7493d6dbbda56
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-16 18:07:42 +0000

    added initial function decleration

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+const countLetters = function(string) {
+  const results = {};
+  
+}
+
+
+string = "Lighthouse Labs";
+
+
+
+
+result1 = countLetters(string) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Test Code 
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫🚫🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// Test Below here
