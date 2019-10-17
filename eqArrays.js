b089a73cfd6406c9d832eca1f891bb6b91a30423
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-17 00:19:25 +0000

    refactor of eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,31 +7,21 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  let string1 = "";
-  let string2 = "";
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++){
-    string1 += array1[i];
-    string2 += array2[i];
-    if (typeof array1[i] !== typeof array2[i]){
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return false;
     }
   }
-  if (string1 === string2) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 };
+
 
 
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false) // => false
+assertEqual(eqArrays([false, "2", "3"], [false, "2"]), false) // => false
+assertEqual(eqArrays([false, "2", "3"], [false, "2", "3"]), true) // => true
