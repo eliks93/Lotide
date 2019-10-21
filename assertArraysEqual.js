80f0919a4f5e82652be1b95f1815d99cbda93121
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-21 16:50:49 +0000

    refactored assertArraysEqual and added test in test folder

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,4 @@
-const eqArrays = function(array1, array2) {
-  let string1 = "";
-  let string2 = "";
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++){
-    string1 += array1[i];
-    string2 += array2[i];
-    if (typeof array1[i] !== typeof array2[i]){
-      return false;
-    }
-  }
-  if (string1 === string2) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(input1, input2) {
   let answer = eqArrays(input1, input2)
@@ -27,4 +9,4 @@ const assertArraysEqual = function(input1, input2) {
   }
 };
 
-assertArraysEqual(["one", 2, 3], ["one", 2, 3]);
+module.exports = assertArraysEqual;
